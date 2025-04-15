668cd3b9141db5d948d54b1665212cd29bbd0693
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-15 16:49:46 +0300

    feat: add model for historical exchange rate object

---
+++ server/CurrencyExchange.Domain/Models/ExternalHistoricalResponse.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace CurrencyExchange.Domain.Models
+{
+    public class ExternalHistoricalResponse
+    {
+        public string Result { get; set; } = string.Empty;
+        public string Documentation { get; set; } = string.Empty;
+        public string Terms_of_use { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public string Base_code { get; set; } = string.Empty;
+        public Dictionary<string, decimal> Conversion_rates { get; set; } = new Dictionary<string, decimal>();
+    }
+}
