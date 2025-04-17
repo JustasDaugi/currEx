a91f98958c0b806506fcab7077341eaf33c6deda
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-17 10:44:24 +0300

    feat(app): add App.tsx root component

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import CurrencyDashboard from "./CurrencyDashboard"
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyDashboard />
+    </div>
+  )
+}
+
+export default App
