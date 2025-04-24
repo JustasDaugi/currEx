59e9ee18275e5bad29a8f3faef4de81e2aaa70c9
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-24 15:43:13 +0300

    refactor(conversion): add BsonRepresentation to Id, switch default! initializers to null!

--- server/CurrencyExchange.Domain/Models/Conversion.cs
+++ server/CurrencyExchange.Domain/Models/Conversion.cs
@@ -1,14 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 
 namespace CurrencyExchange.Domain.Models
 {
   public class Conversion
   {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = default!;
-    public string UserId { get; set; } = default!;
+
+    public string UserId { get; set; } = null!;
     public DateTime Date { get; set; }
-    public string FromCurrency { get; set; } = default!;
-    public string ToCurrency { get; set; } = default!;
+    public string FromCurrency { get; set; } = null!;
+    public string ToCurrency { get; set; } = null!;
     public decimal Amount { get; set; }
     public decimal Rate { get; set; }
     public decimal Result { get; set; }
