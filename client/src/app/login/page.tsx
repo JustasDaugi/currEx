56916292f1e1008fe453e78711e417b317020589
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-24 13:57:25 +0300

    feat(page): add login-form wrapper

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/auth/login";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12">
+      <LoginForm />
+    </div>
+  );
+}
