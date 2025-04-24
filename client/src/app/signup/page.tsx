fde87d2be0017af07e78f1296c0157494240d515
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-24 13:58:07 +0300

    feat(page): add sign-up-form wrapper

---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "@/components/auth/signup";
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12">
+      <SignUpForm />
+    </div>
+  );
+}
