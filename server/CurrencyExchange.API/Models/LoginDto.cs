254f81d8c33497fc65e5b1ae12007e26115e4363
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-24 11:05:33 +0300

    feat(dto): add data transfer object for user login

---
+++ server/CurrencyExchange.API/Models/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace CurrencyExchange.API.Models
+{
+  public class LoginDto
+  {
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+  }
+}
