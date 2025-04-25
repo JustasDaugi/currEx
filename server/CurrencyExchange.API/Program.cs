24353b75dec8610c534eef0ec06d0b65985e230b
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-25 14:03:23 +0300

    feat(auth): bind GoogleAuthSettings & ClientAppSettings

--- server/CurrencyExchange.API/Program.cs
+++ server/CurrencyExchange.API/Program.cs
@@ -1,46 +1,47 @@
+using CurrencyExchange.API.Services;
+using CurrencyExchange.API.Settings;
 using CurrencyExchange.Domain.Repositories;
 using CurrencyExchange.Infrastructure.Repositories;
 using CurrencyExchange.Infrastructure.Settings;
-using CurrencyExchange.API.Services;
-using Microsoft.Extensions.Options;
-using MongoDB.Driver;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MongoSettings>(
     builder.Configuration.GetSection("Mongo"));
 
+builder.Services.Configure<GoogleAuthSettings>(
+    builder.Configuration.GetSection("GoogleAuth"));
+
+builder.Services.Configure<ClientAppSettings>(
+    builder.Configuration.GetSection("ClientApp"));
+
 builder.Services.AddSingleton<IMongoClient>(sp =>
 {
-  var settings = sp.GetRequiredService<IOptions<MongoSettings>>().Value;
-  return new MongoClient(settings.ConnectionString);
+  var opts = sp.GetRequiredService<IOptions<MongoSettings>>().Value;
+  return new MongoClient(opts.ConnectionString);
 });
-
 builder.Services.AddScoped<IConversionRepository, MongoConversionRepository>();
 builder.Services.AddScoped<IUserRepository, MongoUserRepository>();
 
 builder.Services.AddSingleton<JwtTokenService>();
-
-builder.Services.AddControllers();
 builder.Services.AddHttpClient();
+builder.Services.AddControllers();
 
-builder.Services.AddCors(options =>
+builder.Services.AddCors(o => o.AddPolicy("AllowLocalhost3000", p =>
 {
-  options.AddPolicy("AllowLocalhost3000", policy =>
-  {
-    policy
-          .WithOrigins("http://localhost:3000")
-          .AllowAnyHeader()
-          .AllowAnyMethod()
-          .AllowCredentials();
-  });
-});
+  p.WithOrigins("http://localhost:3000")
+   .AllowAnyHeader()
+   .AllowAnyMethod()
+   .AllowCredentials();
+}));
 
 var app = builder.Build();
-
 app.UseRouting();
 app.UseCors("AllowLocalhost3000");
 app.UseAuthorization();
