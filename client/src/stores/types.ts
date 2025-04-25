c7644ecb5ab281c3a1dd0cde626262003d129223
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-25 14:54:37 +0300

    feat(auth): extend Auth types with Google sign-in, state management

--- client/src/stores/types.ts
+++ client/src/stores/types.ts
@@ -1,21 +1,26 @@
 import type { User } from "@/types";
 
 export interface AuthMethods {
-    login(email: string, password: string): Promise<void>;
-    signUp(name: string, email: string, password: string): Promise<void>;
-    logout(): void;
-  }
-  
+  login(email: string, password: string): Promise<void>;
+  signUp(name: string, email: string, password: string): Promise<void>;
+  logout(): void;
+}
 
 export interface AuthContextType extends AuthMethods {
-    user: boolean;
-    isLoading: boolean;
-    signInWithGoogle(): Promise<void>;
-  }
-  
-  export interface AuthState extends AuthMethods {
-    user: User | null;
-    token: string | null;
-    isReady: boolean;
-    fetchUser(): Promise<void>;
-  }
+  user: boolean;
+  isLoading: boolean;
+  signInWithGoogle(): Promise<void>;
+}
+
+export interface AuthState {
+  user: User | null;
+  token: string | null;
+  isReady: boolean;
+
+  fetchUser: () => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  signUp: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => void;
+  setToken: (token: string) => void;
+  setUser: (user: User | null) => void;
+}
